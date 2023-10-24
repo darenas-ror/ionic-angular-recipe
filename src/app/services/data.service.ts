a830576a9bf1b9ef4760ca7f433cbbeb4e2f7518
Daniel Arenas <daniel.arenas@todoscomemos.com>
2023-10-23 22:21:14 -0500

    add diferents ways to save data

--- src/app/services/data.service.ts
+++ src/app/services/data.service.ts
@@ -1,5 +1,15 @@
-import { Injectable, inject } from '@angular/core';
+import { Injectable } from '@angular/core';
 import { Firestore, collectionData, collection } from '@angular/fire/firestore';
+import { addDoc, deleteDoc, doc, updateDoc } from 'firebase/firestore';
+import { docData } from 'rxfire/firestore';
+import { Observable } from 'rxjs';
+
+export interface Recipe {
+  id?: string;
+  name: string;
+  description?: string;
+  ingredients?: string;
+}
 
 @Injectable({
   providedIn: 'root'
@@ -7,8 +17,28 @@ import { Firestore, collectionData, collection } from '@angular/fire/firestore';
 export class DataService {
   constructor(private firestore: Firestore) { }
 
-  getRecipes(){
+  getRecipes(): Observable<Recipe[]> {
     const itemCollection = collection(this.firestore, 'recipes');    
-    return collectionData(itemCollection);
+    return collectionData(itemCollection, { idField: 'id' }) as Observable<Recipe[]>;
+  }
+
+  getRecipeById(id: any): Observable<Recipe[]> {
+    const itemCollection = doc(this.firestore, `recipes/${id}`);    
+    return docData(itemCollection) as Observable<Recipe[]>;
+  }
+
+  AddRecipe(recipe: Recipe) {
+    const itemCollection = collection(this.firestore, 'recipes');    
+    return addDoc(itemCollection, recipe);
+  }
+
+  DeleteRecipe(recipe: Recipe) {
+    const itemCollection = doc(this.firestore, `recipes/${recipe.id}`);
+    return deleteDoc(itemCollection);
+  }
+
+  UpdateRecipe(recipe: Recipe) {
+    const itemCollection = doc(this.firestore, `recipes/${recipe.id}`);
+    return updateDoc(itemCollection, { title: recipe.name, description: recipe?.description, ingredients: recipe?.ingredients });
   }
 }
--- src/app/tab2/tab2.page.ts
+++ src/app/tab2/tab2.page.ts
@@ -1,28 +1,16 @@
 import { Component } from '@angular/core';
 import { DataService } from '../services/data.service';
-import { inject } from '@angular/core';
-import { Firestore, collectionData, collection } from '@angular/fire/firestore';
-import { Observable } from 'rxjs';
-
-interface Item {
-  name?: string
-};
 
 @Component({
   selector: 'app-tab2',
   templateUrl: 'tab2.page.html',
   styleUrls: ['tab2.page.scss']
 })
-
 export class Tab2Page {
 
-  item$: Observable<Item[]>;
-  firestore: Firestore = inject(Firestore);
-
-  constructor() {
-    console.log(" constructor ");
-
-    const itemCollection = collection(this.firestore, 'recipes');
-    this.item$ = collectionData(itemCollection);
+  constructor(private dataService: DataService) {
+    this.dataService.getRecipes().subscribe(res => {
+      console.log(res);
+    })
   }
 }
